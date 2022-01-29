52eebf8b0bbbd7e1141b077e7328cf3a281ea60b
Sebastian Beyer <sebastian.beyer@live.com>
2022-01-29 20:02:00 +0100

    Fix issue by manually copying the wordlists
    Disadvantages are: 1) an extra step is needed and 2) it
    doesn't work with the --onefile option.

--- pack.bat
+++ pack.bat
@@ -1,5 +1,10 @@
 python -m PyInstaller --noconfirm ^
 --hidden-import randomname ^
---onefile ^
 --name RandomNameGenerator ^
 main.py
+
+:: copy the wordlists to the expected location relative to the exe
+set source=.\thirdparty\wordlists
+set destination=.\dist\RandomNameGenerator\randomname\wordlists
+mkdir %destination%
+xcopy %source% %destination% /E /y
